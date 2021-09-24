38c38c63b93f3abec67b893045399de1b8549e4e
chrisss111 <christinecoding10@gmail.com>
2021-09-24 13:44:38 -0400

    created test for add_class method

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,6 +1,16 @@
 from school_schedule.student import Student
+
 def test_student_class_name_is_correct():
   # act
   student = Student("Trenisha", "senior", ["Calculus", "Choir", "Photography", "AP History"])
   # assert
   assert student.name == "Trenisha"
+
+def test_add_class_method_adds_correct_class_name():
+  # act
+  student = Student("Trenisha", "senior", ["Calculus", "Choir", "Photography", "AP History"])
+  class_name = "English"
+
+  student.add_class(class_name)
+
+  assert student.classes == ["Calculus", "Choir", "Photography", "AP History", "English"]
